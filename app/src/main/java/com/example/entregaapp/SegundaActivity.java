241d3b921f5d1f46bd1838bae49a4621af366220
isabellecassiano <isabelle201114@hotmail.com>
2019-07-30 09:02:33 -0300

    Commit de Teste

--- app/src/main/java/com/example/entregaapp/SegundaActivity.java
+++ app/src/main/java/com/example/entregaapp/SegundaActivity.java
@@ -11,6 +11,7 @@ public class SegundaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+        //Teste de Commit
 
         //ALERTDIALOG
 
